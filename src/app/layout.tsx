c1b0fce4597e2d46f11a67aee324269d8de12afa
psyjg14 <psyjg14@nottingham.ac.uk>
2025-02-02 02:44:32 +0000

    Learn Basics and Routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navigation } from "./components/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="bg-slate-900 text-white text-center p-4">
+          {/*<p>Welcome to Next.js 15 Demo</p>*/}
+          <Navigation/>
+        </header>
         {children}
+        <footer className="bg-slate-900 text-white text-center p-4">
+          Jack Gribble
+        </footer>
       </body>
     </html>
   );
